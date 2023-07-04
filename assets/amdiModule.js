dac116bd1d9e075d3166b9c2a9ed312e1ab095bb
unique_hunter <77807213+oguz162@users.noreply.github.com>
2023-07-04 08:05:42 +0300

    Update amdiModule.js

--- assets/amdiModule.js
+++ assets/amdiModule.js
@@ -1,26 +1,3 @@
-/**
-░██████╗░██╗░░░██╗███████╗███████╗███╗░░██╗  ░█████╗░███╗░░░███╗██████╗░██╗
-██╔═══██╗██║░░░██║██╔════╝██╔════╝████╗░██║  ██╔══██╗████╗░████║██╔══██╗██║
-██║██╗██║██║░░░██║█████╗░░█████╗░░██╔██╗██║  ███████║██╔████╔██║██║░░██║██║
-╚██████╔╝██║░░░██║██╔══╝░░██╔══╝░░██║╚████║  ██╔══██║██║╚██╔╝██║██║░░██║██║
-░╚═██╔═╝░╚██████╔╝███████╗███████╗██║░╚███║  ██║░░██║██║░╚═╝░██║██████╔╝██║
-░░░╚═╝░░░░╚═════╝░╚══════╝╚══════╝╚═╝░░╚══╝  ╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═════╝░╚═╝
- __  __       _ _   _       ____             _          
-|  \/  |_   _| | |_(_)     |  _ \  _____   _(_) ___ ___ 
-| |\/| | | | | | __| |_____| | | |/ _ \ \ / / |/ __/ _ \
-| |  | | |_| | | |_| |_____| |_| |  __/\ V /| | (_|  __/
-|_|  |_|\__,_|_|\__|_|     |____/ \___| \_/ |_|\___\___|
-* @project_name Queen Amdi [WA Multi-device]
-* @author BlackAmda <https://github.com/BlackAmda>
-* @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
-* @link <https://github.com/BlackAmda/QueenAmdi>
-* @version 4.0.7
-* @file  amdiModule.js - QueenAmdi bot module and Web WA connection
-
-© 2022 Black Amda, ANTECH. All rights reserved.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.*/
-
 const amdiWA = require('queen_amdi_core/dist/amdiCore');
 const { qrDisplayDL } = require('queen_amdi_core/dist/qrDisplay');
 const amdiWEB = require('queen_amdi_core/qr_code/amdiWEB');
