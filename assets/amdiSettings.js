519392c58fd6ff32ab057255910788e7db53e17e
unique_hunter <77807213+oguz162@users.noreply.github.com>
2023-07-04 08:04:10 +0300

    Update amdiSettings.js

--- assets/amdiSettings.js
+++ assets/amdiSettings.js
@@ -1,26 +1,3 @@
-/**
-░██████╗░██╗░░░██╗███████╗███████╗███╗░░██╗  ░█████╗░███╗░░░███╗██████╗░██╗
-██╔═══██╗██║░░░██║██╔════╝██╔════╝████╗░██║  ██╔══██╗████╗░████║██╔══██╗██║
-██║██╗██║██║░░░██║█████╗░░█████╗░░██╔██╗██║  ███████║██╔████╔██║██║░░██║██║
-╚██████╔╝██║░░░██║██╔══╝░░██╔══╝░░██║╚████║  ██╔══██║██║╚██╔╝██║██║░░██║██║
-░╚═██╔═╝░╚██████╔╝███████╗███████╗██║░╚███║  ██║░░██║██║░╚═╝░██║██████╔╝██║
-░░░╚═╝░░░░╚═════╝░╚══════╝╚══════╝╚═╝░░╚══╝  ╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═════╝░╚═╝
- __  __       _ _   _       ____             _          
-|  \/  |_   _| | |_(_)     |  _ \  _____   _(_) ___ ___ 
-| |\/| | | | | | __| |_____| | | |/ _ \ \ / / |/ __/ _ \
-| |  | | |_| | | |_| |_____| |_| |  __/\ V /| | (_|  __/
-|_|  |_|\__,_|_|\__|_|     |____/ \___| \_/ |_|\___\___|
-* @project_name Queen Amdi [WA Multi-device]
-* @author BlackAmda <https://github.com/BlackAmda>
-* @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
-* @link <https://github.com/BlackAmda/QueenAmdi>
-* @version 4.0.7
-* @file  amdiSettings.js - QueenAmdi config var exports
-
-© 2022 Black Amda, ANTECH. All rights reserved.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.*/
-
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 if (fs.existsSync('amdiSettings.env')) require('dotenv').config({ path: './amdiSettings.env' });
